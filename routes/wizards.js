ec0b0d40b6328252f78490ed5dd252e5772028d5
UlianaAsadulina <u.asadulina@icloud.com>
2025-01-23 10:39:54 -0500

    Add route for search wizards by name in query

--- routes/wizards.js
+++ routes/wizards.js
@@ -6,7 +6,22 @@ const wizards = require("../data/wizards");
 router  
     .route("/")
     .get((req,res) => {
-        res.json(wizards);
+        //route can contain key-value pair for serch by name        
+        const { name } = req.query;//Extract name from query    
+        if (name) { 
+            // can be more then one wizard with the  same last name
+            const selected = wizards.filter(wizard => wizard.firstName == name || wizard.lastName == name);//filter all comments to the post
+            // Check if any comments exist for the postId
+            if (selected.length > 0) {
+                res.json(selected);
+            } else {
+                res.send(`No wizards with name ${name} was found`);
+            };  
+        } else {               
+            //select all wizards if no name in query
+            res.json(wizards); 
+        } 
     });
+
 
 module.exports = router;
